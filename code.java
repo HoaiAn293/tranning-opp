0a5e2486d383a9e7dd153d15ccf01b5e6ea23cb7
HoaiAn293 <lenguyenhoaian293@gmail.com>
2024-02-29 08:50:31 +0700

    push code

---
+++ code.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("John", 25);
+        Car car = new Car("Toyota", "Camry", 2020);
+
+        System.out.println("Person: " + person.getName() + ", Age: " + person.getAge());
+        System.out.println("Car: " + car.getMake() + ", Model: " + car.getModel() + ", Year: " + car.getYear());
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+class Car {
+    private String make;
+    private String model;
+    private int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
