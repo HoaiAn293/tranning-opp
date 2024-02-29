b692b9bc92a9c29f63da944c719480c11bdc2587
HoaiAn293 <lenguyenhoaian293@gmail.com>
2024-02-29 09:21:05 +0700

    calculation class

--- caculation.java
+++ caculation.java
@@ -1,17 +1,23 @@
-def add(a, b):
-    return a + b
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
 
-def subtract(a, b):
-    return a - b
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
 
-def multiply(a, b):
-    return a * b
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
 
-# Example usage
-result_sum = add(5, 3)
-result_minus = subtract(8, 2)
-result_mul = multiply(4, 6)
+    public static void main(String[] args) {
+        int resultSum = add(5, 3);
+        int resultMinus = subtract(8, 2);
+        int resultMul = multiply(4, 6);
 
-print("Sum:", result_sum)
-print("Minus:", result_minus)
-print("Multiplication:", result_mul)
+        System.out.println("Sum: " + resultSum);
+        System.out.println("Minus: " + resultMinus);
+        System.out.println("Multiplication: " + resultMul);
+    }
+}
