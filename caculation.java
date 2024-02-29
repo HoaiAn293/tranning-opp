ea543180f8a1a87e2ffa27a1456d54a9c568663e
HoaiAn293 <lenguyenhoaian293@gmail.com>
2024-02-29 09:19:39 +0700

    calculation class

---
+++ caculation.java
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+# Example usage
+result_sum = add(5, 3)
+result_minus = subtract(8, 2)
+result_mul = multiply(4, 6)
+
+print("Sum:", result_sum)
+print("Minus:", result_minus)
+print("Multiplication:", result_mul)
